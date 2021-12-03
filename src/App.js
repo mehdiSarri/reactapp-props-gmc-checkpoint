0ea5b6f84d89c24c5e5da2b090dc3e9e703d492e
Mehdi Sarri <sarri.mehdi@gmail.com>
2021-12-03 17:04:05 +0100

    Chepoint Perps : Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Profile from './profile/Profile';
+import { useState } from 'react';
 
 
 function App() {
+  
+  const [bio, setBio] = useState("kach bio");
+
+
   return (
     <div className="App">
-      <Profile fullName='React' bio="Hello, My Name is React, Facebook team made me, i'm new and Super COOL">{logo}</Profile>
+      <Profile fullName='React' bio={bio}  setBio={setBio} >{logo}</Profile>
+      
+      <h1>h1 :{bio}</h1>
     </div>
   );
 }
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -3,22 +3,24 @@ import React from "react";
 // {fullName='Default FullName', bio='Default Bio', profession='The default Profession'}
 
 const Profile = (props) => {
+    const { fullName , bio, profession , setBio} = props
+    
+    
     return (
         <>
-
             <img src={props.children} alt={'Alt'}/>
 
-            <h1>{props.fullName}</h1>
-            <h2>{props.bio}</h2>
-            <h3>{props.profession}</h3>
+            <h1 onClick={()=>setBio("hello from profile")} >{fullName}</h1>
+            <h2>{bio}</h2>
+            <h3>{profession}</h3>
         </>
     )
 }
 
-Profile.defaultProps = {
-    fullName: 'New Default fullName',
-    bio: 'New default bio',
-    profession: 'New default profession'
-}
+// Profile.defaultProps = {
+//     fullName: 'New Default fullName',
+//     bio: 'New default bio',
+//     profession: 'New default profession'
+// }
 
 export default Profile
